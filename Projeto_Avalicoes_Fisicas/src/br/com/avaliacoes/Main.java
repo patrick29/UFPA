8fe5ce156173bbe568b377e2e72ba3725539c3e0
Patrick Shibayama <palmeida2909@gmail.com>
2014-10-20 12:01:43 -0300

    Testando

--- Projeto_Avalicoes_Fisicas/src/br/com/avaliacoes/Main.java
+++ Projeto_Avalicoes_Fisicas/src/br/com/avaliacoes/Main.java
@@ -3,7 +3,7 @@ package br.com.avaliacoes;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("");
+		System.out.println("dsf");
 	}
 
 }
--- Projeto_Avalicoes_Fisicas/src/br/com/avaliacoes/Teste.java
+++ Projeto_Avalicoes_Fisicas/src/br/com/avaliacoes/Teste.java
@@ -1,5 +1,4 @@
 package br.com.avaliacoes;
 
 public class Teste {
-
 }
